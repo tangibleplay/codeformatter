2f9e0dff5322c09bd6f4ebe54fbd812eb6fdc47f
Darren Tsung <darren@Darrens-MacBook-Pro-Tangible-Play.local>
2017-03-30 18:08:28 -0700

    Reduce more things besides private this fields

--- src/Microsoft.DotNet.CodeFormatting/Rules/ExplicitThisRule.cs
+++ src/Microsoft.DotNet.CodeFormatting/Rules/ExplicitThisRule.cs
@@ -2,16 +2,16 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Simplification;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Simplification;
 
 namespace Microsoft.DotNet.CodeFormatting.Rules
 {
@@ -43,33 +43,76 @@ namespace Microsoft.DotNet.CodeFormatting.Rules
             {
                 node = (MemberAccessExpressionSyntax)base.VisitMemberAccessExpression(node);
                 var name = node.Name.Identifier.ValueText;
-                if (node.Expression != null &&
-                    node.Expression.Kind() == SyntaxKind.ThisExpression &&
-                    IsPrivateField(node))
+
+                if (node.Expression != null)
                 {
-                    _addedAnnotations = true;
-                    return node.WithAdditionalAnnotations(Simplifier.Annotation);
+					bool shouldReduce = ReducableThisNode(node) || ReducableIdentifierNode(node);
+					if (shouldReduce)
+					{
+	                    _addedAnnotations = true;
+	                    return node.WithAdditionalAnnotations(Simplifier.Annotation);
+					}
                 }
 
                 return node;
             }
 
-            private bool IsPrivateField(MemberAccessExpressionSyntax memberSyntax)
-            {
-                if (_semanticModel == null)
-                {
-                    _semanticModel = _document.GetSemanticModelAsync(_cancellationToken).Result;
-                }
+			private bool ReducableThisNode(MemberAccessExpressionSyntax node)
+			{
+				if (node.Expression.Kind() != SyntaxKind.ThisExpression)
+				{
+					return false;
+				}
 
-                var symbolInfo = _semanticModel.GetSymbolInfo(memberSyntax, _cancellationToken);
-                if (symbolInfo.Symbol != null && symbolInfo.Symbol.Kind == SymbolKind.Field)
-                {
-                    var field = (IFieldSymbol)symbolInfo.Symbol;
-                    return field.DeclaredAccessibility == Accessibility.Private;
-                }
+				if (_semanticModel == null)
+				{
+					_semanticModel = _document.GetSemanticModelAsync(_cancellationToken).Result;
+				}
 
-                return false;
-            }
+				var symbol = _semanticModel.GetSymbolInfo(node, _cancellationToken).Symbol;
+				if (symbol.Kind == SymbolKind.Field || symbol.Kind == SymbolKind.Property)
+				{
+	                var name = node.Name.Identifier.ValueText;
+					if (name.Length <= 0)
+					{
+						return false;
+					}
+
+					// HACK (darren): because we don't explicit this.privateVariable_
+					// but we want to allow for this.gameObject, we shouldn't reduce
+					// variables which don't end with _
+					if (name[name.Length - 1] != '_')
+					{
+						return false;
+					}
+				}
+
+				return true;
+			}
+
+			private bool ReducableIdentifierNode(MemberAccessExpressionSyntax node)
+			{
+				if (node.Expression.Kind() != SyntaxKind.IdentifierName)
+				{
+					return false;
+				}
+
+				if (_semanticModel == null)
+				{
+					_semanticModel = _document.GetSemanticModelAsync(_cancellationToken).Result;
+				}
+
+				var symbol = _semanticModel.GetSymbolInfo(node, _cancellationToken).Symbol;
+				if (symbol != null)
+				{
+					// NOTE (darren): ideally we would want to reduce MyClass.MyPublicMethod() calls
+					// within the same class, but can't figure out how to get the expression's parent Symbol
+					// we can settle for private variables get reduced..
+                    return symbol.DeclaredAccessibility != Accessibility.Public;
+				}
+
+				return false;
+			}
         }
 
         public async Task<SyntaxNode> ProcessAsync(Document document, SyntaxNode syntaxNode, CancellationToken cancellationToken)
